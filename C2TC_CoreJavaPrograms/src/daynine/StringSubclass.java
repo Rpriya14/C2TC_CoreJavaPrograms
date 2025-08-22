fe7e719b8764ee673fe8c3281054768cdd5f55bb
Hp <Hp@LAPTOP-07P0GB5R>
2025-08-22 13:31:38 +0530

    Initial commit

---
+++ C2TC_CoreJavaPrograms/src/daynine/StringSubclass.java
@@ -0,0 +1,11 @@
+//to demonstrate the error
+
+//can't inherit final classes
+package daynine;
+
+public class StringSubclass {
+
+	public static void main(String[] args) {
+		System.out.println("Custom String class - you cannot extend java.lang.String!");
+	}
+}
